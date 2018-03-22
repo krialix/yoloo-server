5a170de3c394d16f4cc1f62ca5fcc5b8cbaeefbf
Yasin Sinan Kayacan <yasinsinan707@gmail.com>
2018-03-23 01:23:47 +0300

    initial commit

---
+++ src/main/kotlin/com/yoloo/server/core/objectify/config/InitializationException.java
@@ -0,0 +1,8 @@
+package com.yoloo.server.core.objectify.config;
+
+public class InitializationException extends RuntimeException {
+
+  public InitializationException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
